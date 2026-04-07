2348a8155c28214d24b97f39e6bffdf8fcf6f6d3
Savannah Rogers <savannah.a.rogers@gmail.com>
2026-04-07 11:53:18 -0700

    functionized for se of linear term

--- demo_script.R
+++ demo_script.R
@@ -15,7 +15,7 @@ set.seed(42) # ALWAYS set a seed if you do any random generation
 err_sigma <- 45 # the true spread of the error
 true_m <- -5 # the true linear relationship
 true_b <- 500 # the tru intercept
-n_data <- 75 # how many data points to simulate
+n_data <- 200 # how many data points to simulate
 
 #---------------------- Functions ----------------------------------------------
 # Define your functions here. Again, sometimes you won't know what you want to
@@ -25,60 +25,100 @@ n_data <- 75 # how many data points to simulate
 
 #---------------------- Do some things and save objects ------------------------
 
-# Simulate an independent variable (since this is all made up anyway, though 
-# normally you'd read this data in from a file)
-xs <- runif(n_data, 0, 100)
-
-# Simulate dependent variable 
-ys <- gen_lin_norm_dat(true_m, xs, true_b, err_sigma)
-
-thedat <- data.frame(x = xs, 
-                     y = ys)
-
-# Fit linear model
-fit <- glm(y ~ x, data = thedat, family = gaussian)
-
-# Get estimator y hat
-mod_b <- fit$coefficients[1]
-mod_m <- fit$coefficients[2]
-y_hat <- mod_m * xs + mod_b
-
-# Get uncertainty
-X <- model.matrix(fit)
-vcov <- vcov(fit)
-sqrt(vcov[2,2]) # standard error of the linear term
-yhat_se <- sqrt(diag(X %*% vcov %*% t(X))) # standard error of each predicted y^
-
-thedat$yhat <- y_hat
-thedat$yhatlow <- y_hat - 1.96 * yhat_se
-thedat$yhatupp <- y_hat + 1.96 * yhat_se
-
-# save if you want to
-saveRDS(thedat, "ex_data.RDS")
-
-#---------------------- Visualize them -----------------------------------------
-
-ggplot() +
-  geom_point(thedat, mapping = aes(x = x,
-                                   y = y)) +
-  geom_line(thedat, mapping = aes(x = x, 
-                                  y = yhat),
-            color = "green3") +
-  geom_ribbon(thedat, mapping = aes(x = x,
-                                    ymin = yhatlow,
-                                    ymax = yhatupp),
-              alpha = .5, fill = "green3") +
-  theme_bw()
+run_linear_model <- function(n_data, # number of data
+                             true_m_f,
+                             true_b_f, 
+                             err_sigma_f
+){
+  # Simulate an independent variable (since this is all made up anyway, though 
+  # normally you'd read this data in from a file)
+  xs <- runif(n_data, 0, 100)
+  
+  # Simulate dependent variable 
+  ys <- gen_lin_norm_dat(true_m, xs, true_b, err_sigma)
+  
+  thedat <- data.frame(x = xs, 
+                       y = ys)
+  
+  # Fit linear model
+  fit <- glm(y ~ x, data = thedat, family = gaussian)
+  
+  # Get estimator y hat
+  mod_b <- fit$coefficients[1]
+  mod_m <- fit$coefficients[2]
+  y_hat <- mod_m * xs + mod_b
+  
+  # Get uncertainty
+  X <- model.matrix(fit)
+  vcov <- vcov(fit)
+  out <- sqrt(vcov[2,2]) # standard error of the linear term
+  return(out)
+}
+# yhat_se <- sqrt(diag(X %*% vcov %*% t(X))) # standard error of each predicted y^
+# 
+# thedat$yhat <- y_hat
+# thedat$yhatlow <- y_hat - 1.96 * yhat_se
+# thedat$yhatupp <- y_hat + 1.96 * yhat_se
+# 
+# # save if you want to
+# saveRDS(thedat, "ex_data.RDS")
+# 
+# #---------------------- Visualize them -----------------------------------------
+# 
+# ggplot() +
+#   geom_point(thedat, mapping = aes(x = x,
+#                                    y = y)) +
+#   geom_line(thedat, mapping = aes(x = x, 
+#                                   y = yhat),
+#             color = "green3") +
+#   geom_ribbon(thedat, mapping = aes(x = x,
+#                                     ymin = yhatlow,
+#                                     ymax = yhatupp),
+#               alpha = .5, fill = "green3") +
+#   theme_bw()
 
 #can save visualizations here if you want, too
 
 # what if I want to do all of this again for 200 data points instead of 75 to 
 # see if this changes the standard error of my prediction?
+run_linear_model(n_data = 200,
+                 true_m_f = true_m, 
+                 true_b_f = true_b,
+                 err_sigma_f = err_sigma)
 
 # what if I want to do that 1000 times to know what the average se is?
+run1000 <- c()
+startt <- Sys.time()
+for(i in 1:1000){
+  run1000[i] <- run_linear_model(n_data = 200,
+                   true_m_f = true_m, 
+                   true_b_f = true_b,
+                   err_sigma_f = err_sigma)
+}
+fortime <- difftime(Sys.time(), startt)
 
-# what if I want to do it 100 times each for different values of n_data?
+starttapply <- Sys.time()
+apply(as.array(1:1000), 1, function(i){
+  out <- run_linear_model(n_data = 200,
+                   true_m_f = true_m, 
+                   true_b_f = true_b,
+                   err_sigma_f = err_sigma)
+  return(out)
+})
+applytime <- difftime(Sys.time(), starttapply)
+sapply(1:1000, )
+lapply(as.list(1:1000))
 
 
+# what if I want to do it 1000 times each for different values of n_data?
+manyn <- sapply(c(50, 75, 200, 500, 1000), function(n_data_a){
+  apply(as.array(1:1000), 1, function(i){
+    out <- run_linear_model(n_data = n_data_a,
+                            true_m_f = true_m, 
+                            true_b_f = true_b,
+                            err_sigma_f = err_sigma)
+    return(out)
+  })
+})
 
 
